7d20a07a4db6b4d271ec82820b55c4499f1529b8
fxyan <1350821504@qq.com>
2018-09-08 10:57:13 +0800

    增加了ajax
    实现了无需刷新的评论

--- private_project/comment/views.py
+++ private_project/comment/views.py
@@ -1,27 +1,31 @@
 from django.shortcuts import render, redirect, reverse
+from django.http import JsonResponse
 from .models import Comment
 from django.contrib.contenttypes.models import ContentType
 from private_project.forms import CommentForm
 
 
 def comment(request):
+    # 一个重定向的地址，如果没有就返回主页
     referer = request.META.get('HTTP_REFERER', reverse('home'))
-    if not request.user.is_authenticated:
-        return render(request, 'error.html', {'message': '用户未登录', 'redirect_to': referer})
-    text = request.POST.get('comment', '').strip()
-    if text == '':
-        return render(request, 'error.html', {'message': '评论不能为空', 'redirect_to': referer})
-    try:
-        user = request.user
-        content_type = request.POST.get('content_type')
-        object_id = int(request.POST.get('object_id'))
-        model_class = ContentType.objects.get(model=content_type).model_class()
-        model_obj = model_class.objects.get(pk=object_id)
-    except Exception as e:
-        return render(request, 'error.html', {'message': '评论对象不存在', 'redirect_to': referer})
-    comment = Comment()
-    comment.user = user
-    comment.text = text
-    comment.content_object = model_obj
-    comment.save()
-    return redirect(referer)
+    comment_form = CommentForm(request.POST, user=request.user)
+    data = {}
+    if comment_form.is_valid():
+        comment = Comment()
+        comment.user = comment_form.cleaned_data['user']
+        comment.text = comment_form.cleaned_data['comment']
+        comment.content_object = comment_form.cleaned_data['content_type']
+        comment.save()
+
+        # 返回数据ajax
+        data['status'] = 'SUCCESS'
+        data['username'] = comment.user.username
+        data['comment_time'] = comment.create_time
+        data['text'] = comment.text
+        # return redirect(referer)
+    else:
+        print('value', list(comment_form.errors.values())[0][0])
+        # return render(request, 'error.html', {'message': comment_form.errors, 'redirect_to': referer})
+        data['status'] = 'ERROR'
+        data['message'] = list(comment_form.errors.values())[0]
+    return JsonResponse(data)
--- private_project/private_project/forms.py
+++ private_project/private_project/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from django.contrib import auth
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import ObjectDoesNotExist
+from ckeditor.widgets import CKEditorWidget
+from comment.models import Comment
 
 
 class LoginForm(forms.Form):
@@ -76,6 +80,29 @@ class CommentForm(forms.Form):
     content_type = forms.CharField(widget=forms.HiddenInput)
     comment = forms.CharField(
         label='评论',
-        max_length=20,
-        widget=forms.Textarea(attrs={'placeholder': '请输入评论'})
+        # 更改小部件使用富文本编辑器
+        widget=CKEditorWidget(config_name='comment_ckeditor'),
+        error_messages={'required': '评论不能为空'}
     )
+
+    # 这里调用的父类的方法将user传了进来方便进行验证super方法还是要再学习学习
+    def __init__(self, *args, **kwargs):
+        if 'user' in kwargs:
+            self.user = kwargs.pop('user')
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        # 判断用户是否登录
+        if self.user.is_authenticated:
+            self.cleaned_data['user'] = self.user
+        else:
+            raise forms.ValidationError('用户尚未登录')
+        object_id = self.cleaned_data['object_id']
+        content_type = self.cleaned_data['content_type']
+        try:
+            model_class = ContentType.objects.get(model=content_type).model_class()
+            model_obj = model_class.objects.get(pk=object_id)
+            self.cleaned_data['content_type'] = model_obj
+        except ObjectDoesNotExist:
+            raise forms.ValidationError('评论对象不存在')
+        return self.cleaned_data
